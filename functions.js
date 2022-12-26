3a2b6c9e639e346b3318bb24665311cefc1f0df8
ndebnam1 <ndebnam1@uncc.edu>
2022-12-26 18:33:25 -0500

    added 'defer'js I added the word defer in the script tage within the head of the index.html file

--- functions.js
+++ functions.js
@@ -1,6 +1,5 @@
 function togglePopup() {
 	document.getElementById("popup-1").classList.toggle("active");
 }
-function togglePopup() {
-	document.getElementById("popup-1").classList.toggle("active");
-}
+
+
