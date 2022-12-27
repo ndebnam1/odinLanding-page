073fc40e96cdafdd17d908e9c2a90d8469687375
ndebnam1 <ndebnam1@uncc.edu>
2022-12-27 13:43:10 -0500

    Corrected all js issues across project I corrected the javascript used for the first 'description' button in my portfolio section of the Landing Page. I also changed the javascript on the buttons in the heading of the page. I altered the github and LinkedIn button to open them in new tabs instead of loading the link in the tab you are currently in. I changed the description and contact me button to close each respective modal if the 'x' used as a closed button is clicked or if the user clicks outside of the modal. I also added a color/opacity to the overlay with each of the popup modals.

--- functions.js
+++ functions.js
@@ -2,4 +2,6 @@ function togglePopup() {
 	document.getElementById("popup-1").classList.toggle("active");
 }
 
-
+function toggleRpsModal() {
+	document.getElementById("rpsModal").classList.toggle("active");
+}
